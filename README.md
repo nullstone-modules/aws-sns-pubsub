9c7e80f7f21e5c712271a8098912ebf75df5b979
Scott Sickles <scottsickles@gmail.com>
2023-12-15 13:39:50 -0500

    squash merge f/1734/sns-topic

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # aws-sns-subscribe
 Connects an application to an SNS topic as a subscriber to receive messages.
+
+This module makes sure to grant the correct access privileges.
+The following environment variables are injected into your application:
+
---
+++ access.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_role_policy_attachment" "access" {
+  role       = var.app_metadata["role_name"]
+  policy_arn = aws_iam_policy.access.arn
+}
+
+resource "aws_iam_policy" "access" {
+  name   = local.resource_name
+  tags   = local.tags
+  policy = data.aws_iam_policy_document.access.json
+}
+
+data "aws_iam_policy_document" "access" {
+  statement {
+    effect    = "Allow"
+    resources = [local.topic_arn]
+
+    actions = [
+      "sns:GetTopicAttributes",
+      "sns:Subscribe"
+    ]
+  }
+
+  dynamic "statement" {
+    for_each = local.kms_key_arn == "" ? [] : [local.kms_key_arn]
+
+    content {
+      effect    = "Allow"
+      resources = [statement.value]
+      actions   = ["kms:Decrypt"]
+    }
+  }
+}
+
+
+/*
+"SNS:GetTopicAttributes",
+"SNS:SetTopicAttributes",
+"SNS:AddPermission",
+"SNS:RemovePermission",
+"SNS:DeleteTopic",
+"SNS:Subscribe",
+"SNS:ListSubscriptionsByTopic",
+"SNS:Publish"
+*/
